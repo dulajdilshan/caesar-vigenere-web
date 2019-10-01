5dd030eeed40c48cc839440a668426b61a07eb36
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-10-02 05:01:27 +0530

    HTML for cipher

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/cvw_db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+# 
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+
+class Cipher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    plaintext = db.Column(db.String(255), nullable=False)
+    key = db.Column(db.String(32), nullable=True)
+    method = db.Column(db.String(10), nullable=False)
+    nochar = db.Column(db.Integer, nullable=True)
+    rotby = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Cipher %r>' % self.id
+
+
+db.create_all()  # create all the tables
 
 
 # db.create_all()
@@ -21,16 +36,17 @@ class User(db.Model):
 # db.session.add(admin)
 # db.session.commit()
 
-print User.query.filter_by(username='admin').first().email
+# print User.query.filter_by(username='admin').first().email
+
 
 @app.route('/')
 def index():
-    return 'This is index!'
+    return 'Welcome!'
 
 
-@app.route('/register')
+@app.route('/form')
 def register():
-    return render_template('register.html')
+    return render_template('form/index.html')
 
 
 if __name__ == '__main__':
