b41cf62fae36b3508e3766435cdd1c1827e18946
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-10-02 02:37:15 +0530

    basic implementation of SQLAlchemy is done Changed the interpreter to python2.7

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask,render_template, g
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/cvw_db'
+db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# db.create_all()
+admin = User(username='admin', email='admin@example.com')
+guest = User(username='guest', email='guest@example.com')
+# db.session.add(admin)
+# db.session.commit()
+
+print User.query.filter_by(username='admin').first().email
 
 @app.route('/')
 def index():
     return 'This is index!'
+
 
 @app.route('/register')
 def register():
