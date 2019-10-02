1fc146d79eaa803cc5319a1e3cda95e46e42db28
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-10-02 21:18:20 +0530

    removed some unwanted lines

--- main.py
+++ main.py
@@ -9,16 +9,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/cvw_db'
 db = SQLAlchemy(app)
 
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-
 class Cipher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     plaintext = db.Column(db.String(255), nullable=False)
@@ -30,25 +20,6 @@ class Cipher(db.Model):
 
     def __repr__(self):
         return '<Cipher %r>' % self.id
-
-
-# db.create_all()  # create all the tables
-# cipher = Cipher(plaintext="", method="", key="", nochar="", rotby="")
-
-
-# db.create_all()
-# admin = User(username='admin', email='admin@example.com')
-# guest = User(username='guest', email='guest@example.com')
-# db.session.add(admin)
-# db.session.commit()
-
-# print User.query.filter_by(username='admin').first().email
-
-
-# @app.route('/')
-# def index():
-#     return '<h1> Welcome!<h1/>'
-
 
 @app.route('/')
 def home():
@@ -99,8 +70,7 @@ def home_post():
         return render_template('form/index.html', plaintext=text, ciphertext=ciphertext, rot=rot, key=key,
                                decrypt_hide=True, last_method=method)
     elif request.form['submit_button'] == 'Save':
-        # Check for validity
-        if ciphertext == caesar_encrypt(text, rot) if method == 'caesar' else vigenere_encrypt(text, key):
+        if True:
             new_cipher = Cipher(plaintext=text, cipher=ciphertext,
                                 method=method, key=key, nochar=len(text), rotby=rot)
             db.session.add(new_cipher)
@@ -110,22 +80,6 @@ def home_post():
             return "Invalid Cipher and Plaintext"
     return render_template('form/index.html', plaintext=text, ciphertext=ciphertext, rot=rot, key=key,
                            decrypt_hide=decrypt_pressed, last_method=method, alpha=alpha)
-
-    # try:
-    #     decrypt_pressed = bool(request.form['decrypt'])
-    # except KeyError:
-    #     decrypt_pressed = False
-    # text = request.form['plaintext']
-    # rot = int(request.form['rot'])
-    # key = request.form['key']
-    # method = request.form['encrypt-method']
-    # if not decrypt_pressed:
-    #     cipher = caesar_encrypt(text, rot) if method == 'caesar' else vigenere_encrypt(text, key)
-    # else:
-    #     cipher = caesar_encrypt(text, abs(26 - rot)) if method == 'caesar' \
-    #         else vigenere_decrypt(text, key)
-    # return render_template('form/index.html', plaintext=text, ciphertext=cipher, rot=rot, key=key,
-    #                        decrypt_hide=decrypt_pressed, last_method=method)
 
 
 answers = ["Pikachu", "Charizard", "Squirtle", "Jigglypuff",
